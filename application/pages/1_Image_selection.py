9d82af52fe68d1a1f9008021ada41621b69b84e5
echtbrecht <brecht.lauwers@outlook.com>
2023-01-22 00:35:07 +0100

    a mvp statemachine

--- application/pages/1_Image_selection.py
+++ application/pages/1_Image_selection.py
@@ -5,31 +5,36 @@ from statemachine import StateMachine, State
 
 class UserExperience(StateMachine):
     "The user experience state machine"
-    choose_picture = State("picture", initial=True)
-    choose_prompt = State("prompt")
+    picture = State("picture", initial=True)
+    prompt = State("prompt")
+    
 
-    picture_selected = choose_picture.to(choose_prompt)
-    prompt_selected = choose_prompt.to(choose_picture)
-
-    def on_enter_choose_picture(self):
-        print("Entered choose picture")
-
-    def on_enter_choose_prompt(self):
-        print("Entered choose prompt")
+    to_prompt = picture.to(prompt)
+    to_picture = prompt.to(picture)
 
 
-steps = UserExperience()
+# Keep state within session
+if "steps" not in st.session_state:
+    st.session_state["steps"] = UserExperience()
+steps = st.session_state["steps"]
 
+st.markdown("""# Welcome!""")
 
-st.markdown("""# Down the rabbit hole 
-                Choose a picture from the Collection Of Ghent""")
+match steps.current_state_value:
+    case "picture":
+        st.markdown("Choose a picture")
+        image = Image.open("pages/fish.jpg")
+        st.image(image)
 
-
-st.button(label="Go to prompt",on_click=steps.picture_selected())
-
-st.button(label="Go to picture",on_click=steps.prompt_selected())
-
-st.markdown(steps.current_state_value)
+        if st.button(label="Go to prompt"):
+            steps.to_prompt()
+            st.experimental_rerun()
+    
+    case "prompt":
+        prompt_input = st.text_input("Fill in your prompt")
+        if st.button(label="Go to picture"):
+            steps.to_picture()
+            st.experimental_rerun()
 
 
 
