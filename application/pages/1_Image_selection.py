37ce0ad48cb052a67c5696238f19146a27f0687d
echtbrecht <brecht.lauwers@outlook.com>
2023-01-22 00:50:36 +0100

    a state machine with two options

--- application/pages/1_Image_selection.py
+++ application/pages/1_Image_selection.py
@@ -7,10 +7,13 @@ class UserExperience(StateMachine):
     "The user experience state machine"
     picture = State("picture", initial=True)
     prompt = State("prompt")
-    
+    digestion = State("digestion")
 
-    to_prompt = picture.to(prompt)
-    to_picture = prompt.to(picture)
+
+    to_prompt = picture.to(prompt) | digestion.to(prompt)
+    to_digestion = prompt.to(digestion)
+    to_picture = digestion.to(picture)
+
 
 
 # Keep state within session
@@ -31,9 +34,20 @@ match steps.current_state_value:
             st.experimental_rerun()
     
     case "prompt":
-        prompt_input = st.text_input("Fill in your prompt")
-        if st.button(label="Go to picture"):
+        prompt_input = st.text_input("Fill in your prompt",value="a fish on a white background in pointilism")
+        if st.button(label="Digest!"):
+            steps.to_digestion()
+            st.experimental_rerun()
+    
+    case "digestion":
+        image = Image.open("pages/fish_digested.png")
+        st.image(image)
+        if st.button(label="Back to start"):
             steps.to_picture()
+            st.experimental_rerun()
+        
+        if st.button(label="Back to prompt"):
+            steps.to_prompt()
             st.experimental_rerun()
 
 
