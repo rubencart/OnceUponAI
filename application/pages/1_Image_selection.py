d5e9166cf5f831771030cfbae717183fe078021b
echtbrecht <brecht.lauwers@outlook.com>
2023-01-21 18:41:33 +0100

    a not working statemachine - probably the buttons actuate before they are clicked on?

--- application/pages/1_Image_selection.py
+++ application/pages/1_Image_selection.py
@@ -1,10 +1,41 @@
 import streamlit as st
 from PIL import Image
+from statemachine import StateMachine, State
 
-st.markdown("""# Down the rabbit hole - 
+
+class UserExperience(StateMachine):
+    "The user experience state machine"
+    choose_picture = State("picture", initial=True)
+    choose_prompt = State("prompt")
+
+    picture_selected = choose_picture.to(choose_prompt)
+    prompt_selected = choose_prompt.to(choose_picture)
+
+    def on_enter_choose_picture(self):
+        print("Entered choose picture")
+
+    def on_enter_choose_prompt(self):
+        print("Entered choose prompt")
+
+
+steps = UserExperience()
+
+
+st.markdown("""# Down the rabbit hole 
                 Choose a picture from the Collection Of Ghent""")
 
 
-image = Image.open('pages/fish.jpg')
-st.image(image, caption="A fish")
+st.button(label="Go to prompt",on_click=steps.picture_selected())
+
+st.button(label="Go to picture",on_click=steps.prompt_selected())
+
+st.markdown(steps.current_state_value)
+
+
+
+
+
+
+
+
 
--- python_environment/conda_create_environment.bat
+++ python_environment/conda_create_environment.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ECHO Creating environment
-conda env create --prefix ./env --file environment.yml --force
+conda env create --prefix ./env --file environment.yml python=3.10 --force
 pause
