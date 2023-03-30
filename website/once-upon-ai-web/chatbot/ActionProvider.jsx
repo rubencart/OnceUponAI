dcc3a8b0f4111be598c844a91595a9707b9f3e7a
Bram Nouwen <nouwenbram@gmail.com>
2023-03-30 19:40:49 +0200

    WIP: OpenAI api integration

--- website/once-upon-ai-web/chatbot/ActionProvider.jsx
+++ website/once-upon-ai-web/chatbot/ActionProvider.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+async function sendMessageToGPT3(prompt) {
+  try {
+    const response = await fetch("/api/gpt3", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: prompt }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const generatedMessage = data.message;
+      console.log("Generated message:", generatedMessage);
+      return generatedMessage;
+    } else {
+      console.error("Error:", response.statusText);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+}
+
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
   const addResponse = (botMessage) => {
     setState((prev) => ({
@@ -13,9 +38,9 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     addResponse(botMessage);
   };
 
-  const handleGPT = (message) => {
+  const handleGPT = async (message) => {
     // TODO: Get GPT response
-    const response = `Contacting GPT for "${message}"`;
+    const response = await sendMessageToGPT3(message);
     const botMessage = createChatBotMessage(response);
     addResponse(botMessage);
   };
---
+++ website/once-upon-ai-web/pages/api/gpt3.js
@@ -0,0 +1,58 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const prompt = req.body.prompt;
+  console.log("Calling gpt3 api with prompt:", prompt);
+
+  const apiKey = "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";
+  // const apiUrl = "https://api.openai.com/v1/completions"; // GPT
+  const apiUrl = "https://api.openai.com/v1/chat/completions"; // ChatGPT
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  // GPT
+  // const requestBody = JSON.stringify({
+  //   model: "text-davinci-003",
+  //   prompt,
+  //   max_tokens: 100,
+  // });
+
+  // ChatGPT
+  const requestBody = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a Dutch speaking city guide from Gent.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    max_tokens: 100,
+  });
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: headers,
+      body: requestBody,
+    });
+    console.log("gpt3 api response", response);
+
+    if (response.ok) {
+      const data = await response.json();
+      // const generatedMessage = data.choices[0].text.trim(); // GPT response
+      const generatedMessage = data.choices[0].message.content.trim(); // ChatGPT response
+      res.status(200).json({ message: generatedMessage });
+    } else {
+      res.status(response.status).json({ error: response.statusText });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
