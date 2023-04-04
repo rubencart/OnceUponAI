98568b02d9a8b67180b2a27c416596f4112bfb0d
rubencart <ruben.cartuyvels@gmail.com>
2023-04-04 14:21:23 +0200

    download objects and download images scripts

--- api_exploration/download_images.py
+++ api_exploration/download_images.py
@@ -1,9 +1,7 @@
 import argparse
-import itertools
 import multiprocessing
 import os
 import socket
-import re
 import traceback
 from argparse import ArgumentParser
 from collections import Counter
@@ -17,76 +15,114 @@ from tqdm import tqdm
 import utils
 
 
-def work(object: Dict, args: argparse.Namespace) -> bool:
+def work(object_id: str, object: Dict, args: argparse.Namespace) -> bool:
     # if possible, download the image from the manifest url in the object dict
     #  see https://ronallo.com/iiif-workshop-new/image-api/uri-parameters.html
 
+    # find the manifest url in the object data
     manifest_url = utils.get_manifest_url_from_obj(object)
+    # query it
+    manifest_response = httpx.get(manifest_url, follow_redirects=True).json()
+
+    # find the image url in the manifest response
     try:
-        manifest_response = httpx.get(manifest_url, follow_redirects=True).json()
         if 'sequences' in manifest_response:
             canv = manifest_response['sequences'][0]['canvases'][0]
             if 'images' in canv:
-                img_dict = canv[0]
+                img_dict = canv['images'][0]
                 img_url = img_dict['resource']['service']['@id']
+
                 # add image formatting params
                 img_url += '/' if img_url[-1] != '/' else ''
                 # region/w,h/rotation/quality.format
-                img_url += 'full/400,/0/default.jpg'
+                # (scales the height proportionally)
+                img_url += f'full/{args.image_width},/0/default.jpg'
 
+                # get the image
+                response = httpx.get(img_url)
 
+                # save the image
+                # filename = f"{manifest_url.split('/')[-1].replace(':', '__')}.jpg"
+                if 'http' in object_id:
+                    oid_split = object_id.split('/')
+                    filename = f"{oid_split[-2]}_{oid_split[-1]}.jpg"
+                else:
+                    filename = f"{object_id}.jpg"
+
+                path = os.path.join(args.output_dir, filename)
+                with open(path, 'wb') as f:
+                    f.write(response.content)
 
                 return True
     except Exception as e:
-        print(e, traceback.format_exc())
+        print(object_id, e, traceback.format_exc())
         return False
     return False
 
 
 def run(args: argparse.Namespace):
 
+    # load the json with the object information
     with open(args.input_objects) as f:
         dsets = json.load(f)
 
+    failed = {}
+    # distribute the downloading work over a pool of worker processes
     with multiprocessing.Pool(args.num_workers) as pool:
+        print('Started MP pool with %s workers' % args.num_workers)
 
         for dset_name, obj_dict in dsets.items():
+            print(dset_name)
 
-            objects = list(obj_dict.items())
-            result = pool.starmap(work,
-                                  tqdm(zip(objects, repeat(args)), total=len(objects)),
-                                  chunksize=args.chunksize)
+            if len(obj_dict) > 0:
+                object_ids, objects = zip(*obj_dict.items())
 
-            counts = Counter(result)
-            print('# images downloaded for collection %s: %s' % (dset_name, counts[True]))
+                if args.debug:  # if debugging, don't use parallel processes
+                    result = []
+                    for oid, obj in tqdm(zip(object_ids, objects)):
+                        result.append(work(oid, obj, args))
 
-    print('ok')
+                else:
+                    result = pool.starmap(work,
+                                          tqdm(zip(object_ids, objects, repeat(args)), total=len(objects)),
+                                          chunksize=args.chunksize)
+
+                counts = Counter(result)
+                print('# images downloaded for collection %s: %s' % (dset_name, counts[True]))
+                print('# images download failed for collection %s: %s' % (dset_name, counts[False]))
+
+                if counts[False] > 0:
+                    failed_ids = [oid for (oid, downloaded) in zip(object_ids, result) if downloaded]
+                    failed[dset_name] = failed_ids
+
+    if len(failed) > 0:
+        failed_path = os.path.join(args.input_objects.split['/'][0], 'failed_ids.json')
+        print('Saving failed ids to %s' % failed_path)
+        with open(failed_path, 'w') as f:
+            json.dump(failed_ids, f)
+
+    print('Done.')
 
 
 if __name__ == '__main__':
     p = ArgumentParser()
-    # p.add_argument('--run_name', type=str, default='debug')
-    # p.add_argument('--in_dir', type=str, default='')
-    # p.add_argument('--in_name', type=str, default='')
-    # p.add_argument('--not_in_dir', type=str, default='NOT_IN_DIR')
-    # p.add_argument('--start', type=int, default=-1)
-    # p.add_argument('--filter_errors', action='store_true')
-    # p.add_argument('--errors', type=int, default=0)
-    # p.add_argument('--save_train_predictions', type=int, default=1)
-
     p.add_argument('--debug', action='store_true')
     p.add_argument('--output_dir', type=str, default='')
-    p.add_argument('--num_workers', type=int, default=0)
-    p.add_argument('--chunk_size', type=int, default=1000)
-    obj_path = '/cw/liir/NoCsBack/testliir/rubenc/OUAI-pt2/LDES-API/api_exploration/output/most_recent_objects_debug.json'
-    p.add_argument('--input_objects', type=str, default='output/most_recent_objects.json')
+    p.add_argument('--num_workers', type=int, default=10, help='Amount of parallel worker processes to use. Minimum: 1.')
+    p.add_argument('--chunksize', type=int, default=50)
+    p.add_argument('--image_width', type=int, default=300)
+
+    obj_path = '/cw/liir/NoCsBack/testliir/rubenc/OUAI-pt2/LDES-API/api_exploration/output/most_recent_objects.json'
+    p.add_argument('--input_objects', type=str, default=obj_path)
     args = p.parse_args()
 
     if args.output_dir == '':
         host = socket.gethostname()
         homedir = 'home1' if host in ['frodo', 'rose'] else 'home2'
-        args.output_dir = os.path.join('/export/%s/NoCsBack/working/' % homedir, '/output/images')
+        args.output_dir = '/export/%s/NoCsBack/working/rubenc/OUAI-Gent/images%s/' \
+                          % (homedir, '_debug' if args.debug else '')
 
         utils.mkdir_p(args.output_dir)
+    print('Using output dir %s' % args.output_dir)
 
     run(args)
--- api_exploration/i_hate_notebooks.py
+++ api_exploration/i_hate_notebooks.py
@@ -1,11 +1,7 @@
 import argparse
-import itertools
-import multiprocessing
 import os
 import re
-import traceback
 from argparse import ArgumentParser
-from itertools import repeat
 from typing import Dict, Tuple, Optional, Set
 
 import httpx
@@ -20,19 +16,8 @@ def get_next_url(next_resp: Dict) -> Tuple[Optional[str], Optional[int]]:
     return None, 0
 
 
-def get_ids_from_response(resp: Dict) -> Set[str]:
-    # todo sometimes there are more than 1 ids, from different systems. We should use 1 system
-    return set(
-        o['Object.identificator'][0]['Identificator.identificator']['@value'] for o in resp['@included']
-    )
-
-
 def get_urls_from_response(resp: Dict) -> Set[str]:
     return set(o['dcterms:isVersionOf'] for o in resp['@included'])
-
-
-def work():
-    pass
 
 
 def run(args: argparse.Namespace):
@@ -49,36 +34,32 @@ def run(args: argparse.Namespace):
         https://apidg.gent.be/opendata/adlib2eventstream/v1/api-docs/
 
     """
+    # query the main API endpoint
     base = "https://apidg.gent.be/opendata/adlib2eventstream/v1/"
     dataset = httpx.get(base).json()
 
+    # regex to extract dataset name
     rgx = r"https:\/\/apidg.gent.be\/opendata\/adlib2eventstream\/v1\/([\/a-zA-Z]+)"
+    # query the endpoint for each of the main dataset collections
     first_responses = {
         re.match(rgx, d['tree:view']).group(1): [
             httpx.get(d["tree:view"], follow_redirects=True).json()
         ]
         for d in dataset["Datasetcatalogus.heeftDataset"]
     }
-    obj_ids = {n: set() for n in first_responses}
     unique_urls = {n: set() for n in first_responses}
 
     # todo we're assuming the API returns objects in newest first order, so to get the most recent version of an
     #  object, we just keep the one that is returned first
     unique_objects = {n: {} for n in first_responses}
 
-    # rgx = r"https:\/\/apidg.gent.be\/opendata\/adlib2eventstream\/v1\/([\/a-zA-Z]+)\?generatedAtTime=[\S]*"
-
+    # for every collection, keep querying the endpoint as long as the last response has a 'lessThanRelation'
     for n, resp_list in first_responses.items():
         if 'thesaurus' in n or 'personen' in n: continue
         if 'tentoonstellingen' in n:            continue
-        # if 'archiefgent' in n:                  continue
         print(n)
 
         next_resp = resp_list[0]
-
-        # obj_ids[n] = obj_ids[n].union(get_ids_from_response(next_resp))
-        # unique_urls[n] = unique_urls[n].union(get_urls_from_response(next_resp))
-
         next_node_url, total_items = get_next_url(next_resp)
 
         remaining_items = total_items
@@ -86,116 +67,56 @@ def run(args: argparse.Namespace):
         url_duplicates = 0
         i = 0
 
-        # with multiprocessing.Pool(args.num_workers) as pool:
-        #     result = pool.starmap(work,
-        #                           tqdm(zip(lines, repeat(num_words), repeat(tg_encoding), repeat(rb)),
-        #                                total=total),
-        #                           chunksize=chunksize)
-
-
-        # try:
+        # progress bar
         with tqdm(total=total_items) as pbar:
             while next_node_url is not None:
                 i += 1
                 if args.debug and i >= 20: break
 
-                # next_node_url, next_remaining_items = get_next_url(next_resp)
-                # pbar.update(remaining_items - next_remaining_items)
-                # remaining_items = next_remaining_items
-
-                # next_resp = httpx.get(next_node_url, follow_redirects=True).json()
-
-                # resp_list.append(next_resp)     # we can drop this to save memory
-
-                # new_ids = get_ids_from_response(next_resp)
-                # duplicates += len(new_ids.intersection(obj_ids[n]))
-                # obj_ids[n] = obj_ids[n].union(new_ids)
-
+                # some bookkeeping to count how many 'not-most-recent' versions objects have
                 urls_in_resp = get_urls_from_response(next_resp)
                 new_urls = urls_in_resp.difference(unique_urls[n])
                 url_duplicates += len(urls_in_resp) - len(new_urls)
-                # url_duplicates += len(new_urls.intersection(unique_urls[n]))
                 unique_urls[n] = unique_urls[n].union(new_urls)
 
+                def url_to_id(url_id):
+                    oid_split = url_id.split('/')
+                    return f"{oid_split[-2]}_{oid_split[-1]}"
+
                 new_objects = {
-                    obj['dcterms:isVersionOf']: obj for obj in next_resp['@included']
+                    url_to_id(obj['dcterms:isVersionOf']): obj
+                    for obj in next_resp['@included']
                     # if obj['dcterms:isVersionOf'] not in unique_urls[n]
                     if obj['dcterms:isVersionOf'] in new_urls
                 }
-                # new_urls = new_objects.keys()
-                # url_duplicates += len(unique_urls[n].intersection(new_urls))
-                # unique_urls[n] = unique_urls[n].union(new_urls)
 
-                # unique_objects[n] = {
-                #     **{
-                #         url: {
-                #             'object': obj,
-                #             'manifest_url': get_manifest_url_from_obj(obj),
-                #         }
-                #         for url, obj in new_objects.items()
-                #       },
-                #     # do this last so objects that were saved already override new objects (newest first)
-                #     #  (doesn't matter anymore now)
-                #     **unique_objects[n],
-                # }
-                # unique_objects[n].update({
-                #     url: {
-                #         'object': obj,
-                #         'manifest_url': get_manifest_url_from_obj(obj),
-                #     }
-                #     for url, obj in new_objects.items()
-                # })
                 unique_objects[n].update(new_objects)
 
                 next_resp = httpx.get(next_node_url, follow_redirects=True).json()
+                # will be None when all objects in a collection have been returned
                 next_node_url, next_remaining_items = get_next_url(next_resp)
+                # update the progress bar
                 pbar.update(remaining_items - next_remaining_items)
                 remaining_items = next_remaining_items
 
-        # except Exception as e:
-        #     print(e)
-        #     traceback.print_exc()
-        #
-        # finally:
         print('# duplicates found for %s: %s' % (n, duplicates))
         print('# unique objects found for %s: %s' % (n, len(unique_objects[n])))
         # nb_manifests = len([obj['manifest_url'] for (oid, obj) in unique_objects[n].items()
         #                     if obj['manifest_url'] is not None])
         # print('# manifest urls found for %s: %s' % (n, nb_manifests))
 
-    p = os.path.join(args.output_dir, 'most_recent_objects.json')
+    p = os.path.join(args.output_dir, 'most_recent_objects_v2.json')
     with open(p, 'w') as f:
         json.dump(unique_objects, f)
     print('Saved objects to %s' % p)
 
-    # obj0_next = objects[0]
-    # obj0_ids = []
-    # for _ in range(20):
-    #     obj0_ids += [o['Object.identificator'][0]['Identificator.identificator']['@value'] for o in
-    #                  obj0_next['@included']]
-    #     obj0_next = httpx.get(obj0_next['tree:relation'][0]["tree:node"], follow_redirects=True).json()
-    # obj0_ids = set(obj0_ids)
-
-    # with open('../../../OUAI-Gent/output2.json', 'r') as f:
-    #     data = json.load(f)
-
-    print('ok')
+    print('Done.')
 
 
 if __name__ == '__main__':
     p = ArgumentParser()
-    # p.add_argument('--run_name', type=str, default='debug')
-    # p.add_argument('--in_dir', type=str, default='')
-    # p.add_argument('--in_name', type=str, default='')
-    # p.add_argument('--not_in_dir', type=str, default='NOT_IN_DIR')
-    # p.add_argument('--start', type=int, default=-1)
-    # p.add_argument('--filter_errors', action='store_true')
-    # p.add_argument('--errors', type=int, default=0)
-    # p.add_argument('--save_train_predictions', type=int, default=1)
-
     p.add_argument('--debug', action='store_true')
     p.add_argument('--output_dir', type=str, default='output/')
-    p.add_argument('--num_workers', type=int, default=0)
     args = p.parse_args()
 
     run(args)
