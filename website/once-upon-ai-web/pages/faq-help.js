338c64caa45454b3b7e3a1d433f8923ccfca2bc4
Bram Nouwen <nouwenbram@gmail.com>
2023-04-11 11:41:52 +0200

    Add items to FAQ & Help

--- website/once-upon-ai-web/pages/faq-help.js
+++ website/once-upon-ai-web/pages/faq-help.js
@@ -7,10 +7,49 @@ const CenterContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 32px;
+  justify-content: center;
+  gap: 16px;
+  height: 80vh;
+`;
+
+const FaqItem = styled.div`
+  margin: 8px 0;
+
+  h2,
+  p {
+    font-size: 0.9rem;
+  }
 `;
 
 export default function FaqHelp() {
+  const FaqItems = [
+    {
+      question: "Why?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nisi bibendum risus iaculis vestibulum.",
+    },
+    {
+      question: "How does this work?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nisi bibendum risus iaculis vestibulum.",
+    },
+    {
+      question: "What is ChatGPT?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nisi bibendum risus iaculis vestibulum.",
+    },
+    {
+      question: "Is my personal data safe?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nisi bibendum risus iaculis vestibulum.",
+    },
+    {
+      question: "Other questions?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu nisi bibendum risus iaculis vestibulum.",
+    },
+  ];
+
   return (
     <div>
       <Head>
@@ -23,6 +62,14 @@ export default function FaqHelp() {
         <WidthContainer>
           <CenterContainer>
             <h2>Faq & Help</h2>
+            <div>
+              {FaqItems.map((item, index) => (
+                <FaqItem key={index}>
+                  <h2>{item.question}</h2>
+                  <p>{item.answer}</p>
+                </FaqItem>
+              ))}
+            </div>
           </CenterContainer>
         </WidthContainer>
       </PageContainer>
