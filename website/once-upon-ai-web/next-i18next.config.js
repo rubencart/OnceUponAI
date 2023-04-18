bbeb549924f9582e1ed587b810326b2876f16a22
Bram Nouwen <nouwenbram@gmail.com>
2023-04-18 17:54:41 +0200

    Move translation files

--- website/once-upon-ai-web/next-i18next.config.js
+++ website/once-upon-ai-web/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "nl",
     locales: ["nl", "en"],
     fallbackLng: "nl",
+    localePath: path.resolve("./public/static/locales"),
   },
 };
