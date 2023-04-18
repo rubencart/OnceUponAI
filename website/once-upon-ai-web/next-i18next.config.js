fe3e11632f7dd46753a6e21e84c413e65627a7e5
Bram Nouwen <nouwenbram@gmail.com>
2023-04-18 13:00:00 +0200

    Remove initial code impl.

--- website/once-upon-ai-web/next-i18next.config.js
+++ website/once-upon-ai-web/next-i18next.config.js
@@ -1,15 +1,3 @@
-// const NextI18Next = require("next-i18next").default;
-// const { initReactI18next } = require("react-i18next");
-
-// module.exports = new NextI18Next({
-//   defaultLanguage: "nl",
-//   otherLanguages: ["en"],
-//   fallbackLng: "nl",
-//   i18next: {
-//     use: [initReactI18next],
-//   },
-// });
-
 module.exports = {
   i18n: {
     defaultLocale: "nl",
